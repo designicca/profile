042e912f589ad6f0e873695115f201c1081204c1
designicca <ssyoung6754@gmail.com>
2015-02-24 12:17:03 +0900

    portfolio - details : 페이지 생성 / 본문 삽입

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/blog' => 'home#blog'
   get '/blog/details' => 'home#blog_details'
+  get '/portfolio/details' => 'home#portfolio_details'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
