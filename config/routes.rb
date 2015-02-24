dc9e3e371ca1c19480bc7081b2698cab8793b11d
designicca <ssyoung6754@gmail.com>
2015-02-24 16:20:31 +0900

    about : 파일 생성 및 기본구조

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/portfolio/details' => 'home#portfolio_details'
   get '/portfolio' => 'home#portfolio'
   get '/contact' => 'home#contact'
+  get '/about' => 'home#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
